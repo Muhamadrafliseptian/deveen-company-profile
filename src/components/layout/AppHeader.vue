a8cd1255b366b4fa76ca323eb212cf5025584105
Muhamad Rafli Septian <rafliseptiannn25@gmail.com>
2026-01-01 19:11:41 +0700

    c
    c

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,19 +1,34 @@
 <template>
-  <a-menu mode="horizontal" theme="dark">
-    <a-menu-item key="home">
-      <RouterLink to="/">Home</RouterLink>
-    </a-menu-item>
-    <a-menu-item key="about">
-      <RouterLink to="/about">About</RouterLink>
-    </a-menu-item>
-    <a-menu-item key="services">
-      <RouterLink to="/services">Services</RouterLink>
-    </a-menu-item>
-    <a-menu-item key="portfolio">
-      <RouterLink to="/portfolio">Portfolio</RouterLink>
-    </a-menu-item>
-    <a-menu-item key="contact">
-      <RouterLink to="/contact">Contact</RouterLink>
-    </a-menu-item>
-  </a-menu>
+  <a-layout-header style="display: flex; align-items: center; padding: 0 24px">
+    <div class="logo">
+      <RouterLink to="/">
+        <img src="@/assets/images/logo.png" alt="Deveen Business Systems" style="height: 100px" />
+      </RouterLink>
+    </div>
+
+    <a-menu mode="horizontal" theme="dark" style="flex: 1; justify-content: flex-end">
+      <a-menu-item key="home">
+        <RouterLink to="/">Home</RouterLink>
+      </a-menu-item>
+      <a-menu-item key="about">
+        <RouterLink to="/about">About</RouterLink>
+      </a-menu-item>
+      <a-menu-item key="services">
+        <RouterLink to="/services">Services</RouterLink>
+      </a-menu-item>
+      <a-menu-item key="portfolio">
+        <RouterLink to="/portfolio">Portfolio</RouterLink>
+      </a-menu-item>
+      <a-menu-item key="contact">
+        <RouterLink to="/contact">Contact</RouterLink>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-header>
 </template>
+
+<style scoped>
+.logo img {
+  display: block;
+  max-height: 100px;
+}
+</style>
