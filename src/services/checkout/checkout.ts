090862a0758f792c950b2bea9c212baea448de0f
Muhamad Rafli Septian <rafliseptiannn25@gmail.com>
2026-01-04 03:16:22 +0700

    c
    c

--- src/services/checkout/checkout.ts
+++ src/services/checkout/checkout.ts
@@ -1,10 +1,11 @@
+import axios from 'axios'
+
 export const CheckoutService = {
   async checkoutCp(plan_id: number) {
-    const res = await fetch(`/api/public/checkout`, {
-      method: 'POST',
-      body: JSON.stringify({ plan_id }),
-    });
-    return res.json();
-  }
+    const res = await axios.post(
+      'https://6cb5f5349086.ngrok-free.app/api/checkout',
+      { plan_id }
+    )
+    return res.data
+  },
 }
-
